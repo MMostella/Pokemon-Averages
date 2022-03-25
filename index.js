485d8c17abfc521dc0daec320046aa7e1bd6859c
Mason Mostella <>
2022-03-25 16:38:57 -0500

    Added averaging function

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 #!/usr/bin/env node
 
 import fetch from "node-fetch";
+import lodash from "lodash";
 
 let api_url = 'https://pokeapi.co/api/v2/pokemon';
+let height = [];
+let weight = [];
 
 async function fetchPokemon(limit, offset) {
 
     fetch(`${api_url}?limit=${limit}&offset=${offset}`)
         .then((response) => response.json())
         .then(allPokemon => {
+            // console.log(allPokemon);
             allPokemon.results.forEach(pokemon => {
                 getPokemonData(pokemon, limit)
             })
@@ -17,39 +21,26 @@ async function fetchPokemon(limit, offset) {
 
 function getPokemonData(pokemon, limit) {
     // console.log(pokemon)
-    let height = [];
-    let weight = [];
-    // let averageHeight = height / limit;
-    // let averageWeight = weight / limit;
     let url = pokemon.url
     fetch(url)
     .then(response => response.json())
-    .then(function(pokeData) {
-        // console.log(pokeData)
-        // pokeData.forEach(
-        //     height.push(pokeData.height)
-        // )
-        height.concat(pokeData.height);
-        console.log(pokeData.height)
-        // weight.push(pokeData.weight);
-        // console.log('---')
-        // console.log('Limit: ', limit)
-        // console.log('Height: ', pokeData.height)
-        // console.log('Weight: ', pokeData.weight)
-        // console.log('Height Average: ', averageHeight)
-        // console.log('Weight Average: ', averageWeight)
+    .then(pokeData => {
+        console.log("---")
+        console.log(pokeData.height);
+        console.log(pokeData.weight);
+        height.push(pokeData.height);
+        weight.push(pokeData.weight);
+        pokeAverages(pokeData.height, pokeData.weight, limit);
     })
 }
 
+function pokeAverages(limit) {
+    let heightSum = lodash.sum(height);
+    let weightSum = lodash.sum(weight);
+    let heightAverage = heightSum / limit;
+    let weightAverage = weightSum / limit;
+    console.log("Height Average: ", heightAverage);
+    console.log("Weight Average: ", weightAverage);
+}
+
 await fetchPokemon(2, 30)
-
-
-
-
-// const arr = [1, 2, 3, 4, 5];
-// var sum = 0;
-// for (var number of arr) {
-//     sum += number;
-// }
-// average = sum / arr.length;
-// console.log(average);
