a5a28c8d07e64c03d11fab592ab9100250a894c6
Mason Mostella <>
2022-03-25 18:55:16 -0500

    Added inquirer and sleepers

--- index.js
+++ index.js
@@ -2,49 +2,74 @@
 
 import fetch from "node-fetch";
 import lodash from "lodash";
+import inquirer from "inquirer";
 
 let api_url = 'https://pokeapi.co/api/v2/pokemon';
-let height = [];
-let weight = [];
+var height = [];
+var weight = [];
+let limit;
+let offset;
 
-async function fetchPokemon(limit, offset) {
+const sleep = (ms = 500) => new Promise((r) => setTimeout(r, ms));
+let start = Date.now()
 
+async function askLimit() {
+    const answer = await inquirer.prompt({
+        name: 'limit',
+        type: 'input',
+        message: 'Choose your limit...',
+        default() {
+            return;
+        }
+    })
+    limit = answer.limit;
+}
+
+async function askOffset() {
+    const answer = await inquirer.prompt({
+        name: 'offset',
+        type: 'input',
+        message: 'Choose your offset...',
+        default() {
+            return;
+        }
+    })
+    offset = answer.offset;
+}
+
+async function fetchPokemon() {
     fetch(`${api_url}?limit=${limit}&offset=${offset}`)
         .then((response) => response.json())
         .then(allPokemon => {
-            // allPokemon.results.reduce(pokemon => {
-            //         getPokemonData(pokemon, limit)
-            //     })
-            // console.log(allPokemon);
-            // for (let i = 0; i < allPokemon.results.length; i++) {
-            //     getPokemonData(limit)
-            // }
             allPokemon.results.forEach(pokemon => {
                 getPokemonData(pokemon, limit)
             })
         })
 }
 
-function getPokemonData(pokemon, limit) {
-    // console.log(pokemon)
+function getPokemonData(pokemon) {
     let url = pokemon.url
     fetch(url)
     .then(response => response.json())
     .then(pokeData => {
         height.push(pokeData.height);
         weight.push(pokeData.weight);
-        pokeAverages(pokeData.height, pokeData.weight, limit);
     })
 }
 
-function pokeAverages(limit) {
+async function pokeAverages() {
+    await sleep();
     let heightSum = lodash.sum(height);
     let weightSum = lodash.sum(weight);
     let heightAverage = heightSum / limit;
     let weightAverage = weightSum / limit;
-    console.log('---')
     console.log("Height Average: ", heightAverage);
     console.log("Weight Average: ", weightAverage);
+    let end = Date.now()
+    console.log(`Execution time: ${(end - start) / 1000} seconds`);
 }
 
-await fetchPokemon(2, 0)
+await askLimit();
+await askOffset();
+await fetchPokemon()
+await pokeAverages()
