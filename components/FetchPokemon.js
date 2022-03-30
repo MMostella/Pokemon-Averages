54ab31df97713e948713d18b491809f2e704c7e0
Mason Mostella <>
2022-03-30 16:46:36 -0500

    Created Fetching component

---
+++ components/FetchPokemon.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const FetchPokemon = async (limit, offset) => {
+    let api_url = 'https://pokeapi.co/api/v2/pokemon';
+    let pokemonArray = [];
+    await axios(`${api_url}?limit=${limit}&offset=${offset}`)
+    .then(response => {
+        response.data.results.forEach(pokemon => {
+            pokemonArray.push(pokemon)
+        })
+    })
+    .catch(error => {
+        console.error(error)
+    })
+
+    return pokemonArray;
+}
+
+export default FetchPokemon;
