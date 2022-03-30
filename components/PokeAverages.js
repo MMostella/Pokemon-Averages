b9fe5f9b94e872f3a4d1df07b099524b89757fbd
Mason Mostella <>
2022-03-30 16:47:06 -0500

    Created Averages component

---
+++ components/PokeAverages.js
@@ -0,0 +1,13 @@
+const PokeAverages = (height, weight, limit) => {
+    // console.log(`Height: ${height}`)
+    // console.log(`Weight: ${weight}`)
+    // console.log(`Limit: ${limit}`)
+    // let heightSum = height.reduce((partialSum, a) => partialSum + a, 0);
+    // let weightSum = weight.reduce((partialSum, a) => partialSum + a, 0);
+    // console.log(heightSum);
+    // console.log(weightSum);
+    // console.log("Height Average: ", (heightSum / limit));
+    // console.log("Weight Average: ", (weightSum / limit));
+}
+
+export default PokeAverages;
