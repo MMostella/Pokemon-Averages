d20d9585f47cc187688e077a1ad217e7890e0fa6
Mason Mostella <>
2022-04-01 11:32:34 -0500

    Added description to HandleErrorData test

--- components/HandleErrorData.test.js
+++ components/HandleErrorData.test.js
@@ -5,11 +5,11 @@ const url = "https://pokeapi.co/api/v2/pokemon/2/"
 
 const result = { heightArray: [10], weightArray: [130] }
 
-describe("sanity", () => {
+describe("Getting stats from the indiviual pokemon", () => {
   beforeEach(() => {
     jest.spyOn(axios, "get").mockResolvedValue({ data: { results: result } });
   })
-    it("1 + 1", async () => {
+    it("individual pokemon url goes in, obj with height, weight array is returned", async () => {
       expect(await HandleErrorData(url)).toEqual(result);
     });
   });
