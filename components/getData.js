28f447e95561f15d3494f4d3427800e61cd5fa16
MMostella <masonmostella@gmail.com>
2022-03-31 20:47:49 -0500

    Cleaned index and fixed getData

--- components/getData.js
+++ components/getData.js
@@ -1,28 +1,27 @@
-import axios from "axios"
+import axios from "axios";
 import HandleErrorData from "./HandleErrorData.js";
 
 const GetData = async (pokemon) => {
-    let url = pokemon.url
-    let height = 0;
-    let weight = 0;
-    let first = 0;
-    let second = 0;
-    let third = 0;
-    await axios(url)
-    .then(response => {
-        height += response.data.height;
-        weight += response.data.weight;
-        first += 1;
+  let url = pokemon.url;
+  let height = 0;
+  let weight = 0;
+  let first = 0;
+  let second = 0;
+  await axios(url)
+    .then((response) => {
+      height += response.data.height;
+      weight += response.data.weight;
+      first += 1;
     })
-    .catch(error => {
-        if (error.config.url) {
-        let overflow = HandleErrorData(error.config.url)
+    .catch((error) => {
+      if (error.config.url) {
+        let overflow = HandleErrorData(error.config.url);
         height += overflow.height;
         weight += overflow.weight;
         second += 1;
-        }
-    })
-    return { height: height, weight: weight, first, second } ;
-}
+      }
+    });
+  return { height: height, weight: weight, first, second };
+};
 
 export default GetData;
--- index.js
+++ index.js
@@ -11,46 +11,45 @@ let limit;
 let offset;
 
 const askLimit = async () => {
-    const answer = await inquirer.prompt({
-        name: 'limit',
-        type: 'input',
-        message: 'Choose your limit...',
-        default() {
-            return;
-        }
-    })
-    limit = answer.limit;
-}
+  const answer = await inquirer.prompt({
+    name: "limit",
+    type: "input",
+    message: "Choose your limit...",
+    default() {
+      return;
+    },
+  });
+  limit = answer.limit;
+};
 
 const askOffset = async () => {
-    const answer = await inquirer.prompt({
-        name: 'offset',
-        type: 'input',
-        message: 'Choose your offset...',
-        default() {
-            return;
-        }
-    })
-    offset = answer.offset;
-}
+  const answer = await inquirer.prompt({
+    name: "offset",
+    type: "input",
+    message: "Choose your offset...",
+    default() {
+      return;
+    },
+  });
+  offset = answer.offset;
+};
 
 const startApp = async () => {
-    await askLimit();
-    await askOffset();
-    const spinner = createSpinner("Let me look around...").start();
-    const start = Date.now();
-    let pokemon = await FetchPokemon(limit, offset);
-    let pokemonArray = pokemon.map(async (poke) => {
-        let stats = await GetData(poke);
-        return stats;
-    })
-    const resolved = await Promise.all(pokemonArray);
-    const test = Totals(resolved);
-    console.log(test)
-    spinner.success();
-    const stop = Date.now();
-    // console.log(`Completed in ${(stop - start) / 1000} seconds`)
-    // return PokeAverages(test.heightTotal, test.weightTotal, limit)
-}
+  await askLimit();
+  await askOffset();
+  const spinner = createSpinner("Let me look around...").start();
+  const start = Date.now();
+  let pokemon = await FetchPokemon(limit, offset);
+  let pokemonArray = pokemon.map(async (poke) => {
+    let stats = await GetData(poke);
+    return stats;
+  });
+  const resolved = await Promise.all(pokemonArray);
+  const test = Totals(resolved);
+  spinner.success();
+  const stop = Date.now();
+  console.log(`Completed in ${(stop - start) / 1000} seconds`);
+  return PokeAverages(test.heightTotal, test.weightTotal, limit);
+};
 
 startApp();
